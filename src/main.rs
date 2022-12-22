59e90ca5edaf6c287019ba5d96ccbc21fcc2fc6b
amaco711 <121236146+amaco711@users.noreply.github.com>
2022-12-22 22:37:51 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crate::mangacross::MangaCrossComic;
 
 mod mangacross;
 
-const TARGETS: [&str; 2] = ["yabai", "negasuki"];
+const TARGETS: [&str; 2] = ["yabai", "negasuki", "shiomai"];
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
